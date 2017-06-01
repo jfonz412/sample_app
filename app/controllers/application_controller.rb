e2cbed1d45a616ab35a02277202d9315de0ee593
jfonz412 <jfonzone@outlook.com>
2017-06-01 14:12:40 -0400

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # class probably 'require(s)' helper directory
+  include SessionsHelper # app/helpers/session_helper.rb
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
+	
   def new
   end
 
-  def create # creates session hash w/ form when user signs in
+  # creates session hash w/ form when user signs in
+  def create 
   	user = User.find_by(email: params[:session][:email].downcase)
+
   	if user && user.authenticate(params[:session][:password])
-  		# Log the user in and redirect to the user's show page
+  		log_in user #app/helpers/session_helper.rb
+  		redirect_to user
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'

cdacd09b2a975e6309d37dfb5395e20939e7c134
jfonz412 <jfonzone@outlook.com>
2017-06-01 15:11:44 -0400

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-	
+
   def new
   end
 
@@ -17,5 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+  	log_out
+  	redirect_to root_url
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,15 +16,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert flash.empty? # verify that the flash doesn't appear on new page
 	end
 
-	test "login with valid information" do
+	test "login with valid information followed by logout" do
 		get login_path
 		post login_path, params: { session: { email: 		@user.email,
 																					password: 'password' } }
+		assert is_logged_in?
 		assert_redirected_to @user
 		follow_redirect!
 		assert_template 'users/show'
 		assert_select "a[href=?]", login_path, count: 0 #should be no log in button
 		assert_select "a[href=?]", logout_path
 		assert_select "a[href=?]", user_path(@user)
+		# Log out
+		delete logout_path #send delete request to logout_path
+		assert_not is_logged_in?
+		assert_redirected_to root_path
+		follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
 	end
 end

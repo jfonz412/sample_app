210903ec61283023b3b2bb8f332eee62083b858f
jfonz412 <jfonzone@outlook.com>
2017-06-05 14:29:23 -0400

    display users when logged in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+	def setup
+		@user = users(:michael)
+	end
+
 	test "layout links" do
 		get root_path
 		assert_template 'static_pages/home' # checks proper template has been rendered
@@ -15,5 +19,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		get signup_path
 		assert_template 'users/new'
 		assert_select "title", full_title("Sign up")
+	end
+
+	test "links when logged in" do
+		get root_path
+		log_in_as(@user)
+		follow_redirect!
+		assert_select "a[href=?]", users_path
+		assert_select "a[href=?]", user_path
+		assert_select "a[href=?]", help_path
+		assert_select "a[href=?]", about_path
+		assert_select "a[href=?]", contact_path
+		assert_select "a[href=?]", edit_user_path
+		assert_select "a[href=?]", logout_path
 	end
 end

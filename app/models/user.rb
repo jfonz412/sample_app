e2f3acc49b9745c0841085a436e40b5c3f7325b3
jfonz412 <jfonzone@outlook.com>
2017-06-05 20:51:37 -0400

    activation token and digest for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token # tokens are for the class
+  before_save   :downcase_email
+  before_create :create_activation_digest # digest is stored in the database
 
-	before_save { email.downcase! } # callback
 	validates(:name, presence: true, length:{ maximum: 50 }) # parentheses not necessary
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -39,4 +40,15 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    def downcase_email
+      self.email.downcase!
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end

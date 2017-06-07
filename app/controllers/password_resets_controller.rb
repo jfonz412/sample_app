ef50485c8b215cd8c04d6be41ac84722466639db
jfonz412 <jfonzone@outlook.com>
2017-06-07 14:31:43 -0400

    Add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,6 +29,7 @@ class PasswordResetsController < ApplicationController
   		render 'edit'
   	elsif @user.update_attributes(user_params)
   		log_in @user
+  		@user.update_attribute(:reset_digest, nil) # so user can't go back and change pass again
   		flash[:success] = "Password has been reset."
   		redirect_to @user
   	else

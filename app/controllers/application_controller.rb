a557f1043928b30b902fab0e5f0c678f41f96348
jfonz412 <jfonzone@outlook.com>
2017-06-11 20:05:11 -0400

    prevent from creating/deleting posts when not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # class probably 'require(s)' helper directory
   include SessionsHelper # app/helpers/session_helper.rb
+
+  private
+
+      # Confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end

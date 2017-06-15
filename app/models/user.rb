169ecfecff638b0ec5c239c9e7f9a510454d1abd
jfonz412 <jfonzone@outlook.com>
2017-06-15 16:31:40 -0400

    follow and unfollow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,30 @@
 class User < ApplicationRecord
-  has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id", #Relationship refers to user as follower_id
-                                  dependent:   :destroy       #if user destroyed, destroy relationships
   attr_accessor :remember_token, :activation_token, :reset_token # tokens are for the class
+
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id", #Relationship refers to user as follower_id
+                                   dependent:   :destroy       #if user destroyed, destroy relationships 
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed # acts like an ARRAY (active record ass.)
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   before_save   :downcase_email
   before_create :create_activation_digest # digest is stored in the database
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates(:name, presence: true, length:{ maximum: 50 }) # parentheses not necessary
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
   has_secure_password
-  # passwords are allowed to be nil for the this line, but not for the above method
-  # (upon user creation)
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true  # (upon user creation, i think this is for testing iirc)
+
+
+  # DEFINITIONS
 
   # Returns the hash digest of the given string
   def User.digest(string)
@@ -74,6 +83,18 @@ class User < ApplicationRecord
   # ? can be subsituted for id, but would cause a major security hole 
   def feed
     Micropost.where("user_id = ?", id) # pulls all posts of this user via id
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
